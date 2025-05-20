2a8f908488ab2b9edc295442bdc95249dd78121e
Mateus C. <zamasu.org@gmail.com>
2025-05-19 22:37:00 -0300

    Atualizações Instaveis

--- Buy_Cart/Controllers/HomeController.cs
+++ Buy_Cart/Controllers/HomeController.cs
@@ -1,32 +1,84 @@
-using System.Diagnostics;
-using Buy_Cart.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using CarrinhoCompras.Models;
 
-namespace Buy_Cart.Controllers
+
+namespace CarrinhoComprasMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private const string SessionKey = "ListaCompras";
 
         public IActionResult Index()
         {
-            return View();
+            var listas = GetListasFromSession();
+            ViewBag.TotalPendente = listas.Values.SelectMany(x => x)
+                .Where(i => !i.Comprado)
+                .Sum(i => i.Preco * i.Quantidade);
+
+            return View(listas);
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Adicionar(string nome, int quantidade, decimal preco, string categoria)
         {
-            return View();
+            var listas = GetListasFromSession();
+
+            if (!listas.ContainsKey(categoria))
+                listas[categoria] = new List<ItemCompra>();
+
+            listas[categoria].Add(new ItemCompra
+            {
+                Nome = nome,
+                Quantidade = quantidade,
+                Preco = preco,
+                Categoria = categoria
+            });
+
+            SaveListasToSession(listas);
+            return RedirectToAction(nameof(Index));
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Marcar(Guid id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var listas = GetListasFromSession();
+
+            foreach (var lista in listas.Values)
+            {
+                var item = lista.FirstOrDefault(i => i.Id == id);
+                if (item != null)
+                {
+                    item.Comprado = !item.Comprado;
+                    break;
+                }
+            }
+
+            SaveListasToSession(listas);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Exportar()
+        {
+            var listas = GetListasFromSession();
+            var json = JsonSerializer.Serialize(listas);
+            var bytes = System.Text.Encoding.UTF8.GetBytes(json);
+            return File(bytes, "application/json", "lista_compras.json");
+        }
+
+        private Dictionary<string, List<ItemCompra>> GetListasFromSession()
+        {
+            var json = HttpContext.Session.GetString(SessionKey);
+            if (string.IsNullOrEmpty(json))
+                return new Dictionary<string, List<ItemCompra>>();
+            return JsonSerializer.Deserialize<Dictionary<string, List<ItemCompra>>>(json);
+        }
+
+        private void SaveListasToSession(Dictionary<string, List<ItemCompra>> listas)
+        {
+            var json = JsonSerializer.Serialize(listas);
+            HttpContext.Session.SetString(SessionKey, json);
         }
     }
 }
---
+++ Buy_Cart/Models/ItemCompra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CarrinhoComprasPDF.Models
+{
+    public class ItemCompra
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+        public bool Comprado { get; set; }
+    }
+}
