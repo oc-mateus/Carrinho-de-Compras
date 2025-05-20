8329d38549d4b1dc30ce9a1f9b82e3d7ee840f6c
Mateus C. <zamasu.org@gmail.com>
2025-05-19 21:35:30 -0300

    Adicionar arquivos de projeto.

---
+++ Buy_Cart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buy_Cart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
